61b9134b05030a725640e5873b8aa015e070d1f7
Ashish Karhade <akarhade5@gmail.com>
2024-06-15 20:44:58 +0530

    Single Property Page added

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const API_DOMAIN = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
+// FETCH ALL PROPERTIES
 async function fetchProperties(){
     try {
         if(!API_DOMAIN){
@@ -16,4 +17,22 @@ async function fetchProperties(){
     }
 }
 
-export { fetchProperties };
+
+// FETCH SINGLE PROPERTY
+async function fetchProperty(id){
+    try {
+        if(!API_DOMAIN){
+            return null
+        }
+        const res = await fetch(`${API_DOMAIN}/properties/${id}`);
+        if(!res.ok){
+            throw new Error("Failed to fetch data.");
+        }
+        return res.json();
+    } catch (error){
+        console.error(error);
+        return null
+    }
+}
+
+export { fetchProperties, fetchProperty };
