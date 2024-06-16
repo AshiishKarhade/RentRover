54623e72fa9c4f6e16346a3341d60424810ee67c
Ashish Karhade <akarhade5@gmail.com>
2024-06-16 20:38:40 +0530

    Authentication with Next-auth completed

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     env: {
         MONGODB_URI: process.env.MONGODB_URI,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '**',
+            }
+        ]
+    }
 };
 
 export default nextConfig;
