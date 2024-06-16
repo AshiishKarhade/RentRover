629c2564905be510596599225ff9c80b2937ccaa
Ashish Karhade <akarhade5@gmail.com>
2024-06-16 12:00:35 +0530

    Next-auth Google Provider added

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,24 +2,27 @@ import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "RentRover",
+  title: "RentRover | Find perfect rental properties",
   description: "Property finding made easy",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        <main>
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Navbar />
+          <main>
+            {children}
+          </main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
