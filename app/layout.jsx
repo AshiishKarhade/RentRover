01a4424272214902834ab81e97ef7d4642a7c621
Ashish Karhade <akarhade5@gmail.com>
2024-06-24 19:52:13 +0530

    Add message component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,33 +1,32 @@
-import { Inter } from "next/font/google";
-import "@/assets/styles/globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import AuthProvider from "@/components/AuthProvider";
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
 import { ToastContainer } from 'react-toastify';
-// import { GlobalProvider } from '@/context/GlobalContext';
-// import 'react-toastify/dist/ReactToastify.css';
-// import 'photoswipe/dist/photoswipe.css';
-
-const inter = Inter({ subsets: ["latin"] });
+import { GlobalProvider } from '@/context/GlobalContext';
+import '@/assets/styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+import 'photoswipe/dist/photoswipe.css';
 
 export const metadata = {
-  title: "RentRover | Find perfect rental properties",
-  description: "Property finding made easy",
+    title: 'PropertyPulse | Find The Perfect Rental',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties',
 };
 
-export default function RootLayout({ children }) {
-  return (
-      // <GlobalProvider>
-        <AuthProvider>
-          <html lang='en'>
-              <body>
-              <Navbar />
-              <main>{children}</main>
-              <Footer />
-              <ToastContainer />
-              </body>
-          </html>
-        </AuthProvider>
-      // </GlobalProvider>
-  );
-}
+const MainLayout = ({ children }) => {
+    return (
+        <GlobalProvider>
+            <AuthProvider>
+                <html lang='en'>
+                <body>
+                <Navbar />
+                <main>{children}</main>
+                <Footer />
+                <ToastContainer />
+                </body>
+                </html>
+            </AuthProvider>
+        </GlobalProvider>
+    );
+};
+export default MainLayout;
--- components/Properties.jsx
+++ components/Properties.jsx
@@ -7,15 +7,15 @@ import Pagination from '@/components/Pagination';
 const Properties = () => {
     const [properties, setProperties] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [page, setPage] = useState(1);
-    const [pageSize, setPageSize] = useState(6);
+    // const [page, setPage] = useState(1);
+    // const [pageSize, setPageSize] = useState(6);
     const [totalItems, setTotalItems] = useState(0);
 
     useEffect(() => {
         const fetchProperties = async () => {
             try {
                 const res = await fetch(
-                    '/api/bookmarks'
+                    `/api/properties`
                 );
 
                 if (!res.ok) {
@@ -23,7 +23,7 @@ const Properties = () => {
                 }
 
                 const data = await res.json();
-                setProperties(data);
+                setProperties(properties);
                 // setTotalItems(data.total);
             } catch (error) {
                 console.log(error);
@@ -33,20 +33,18 @@ const Properties = () => {
         };
 
         fetchProperties();
-    }, []);
+    }, [properties]);
 
-    console.log(properties);
-
-    // const handlePageChange = (newPage) => {
-    //     setPage(newPage);
-    // };
+    const handlePageChange = (newPage) => {
+        setPage(newPage);
+    };
 
     return loading ? (
         <Spinner />
     ) : (
         <section className='px-4 py-6'>
             <div className='container-xl lg:container m-auto px-4 py-6'>
-                { properties.length === 0 ? (
+                {properties.length === 0 ? (
                     <p>No properties found</p>
                 ) : (
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
